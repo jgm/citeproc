14dd1e304fedbb13e0393441bb34160d74b6f2dc
John MacFarlane <jgm@berkeley.edu>
2020-09-09 10:37:19 -0700

    Reverted a spurious change introduced in last commit.

--- src/Citeproc/Eval.hs
+++ src/Citeproc/Eval.hs
@@ -1049,7 +1049,7 @@ getPosition item groupNum mbNoteNum posInGroup = do
             else id) .
         (if (groupNum == prevGroupNum &&
              posInGroup == prevPosInGroup + 1) ||
-            (groupNum == prevGroupNum + 1 &&
+            (noteNum == prevNoteNum + 1 &&
              posInGroup == 1 &&
              prevAloneInGroup)
              then case (prevLoc, mbloc) of
