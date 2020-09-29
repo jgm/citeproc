1f25d7b5bbe7d5137a66a12f2c2caf11e6cf4e54
John MacFarlane <jgm@berkeley.edu>
2020-09-29 10:43:18 -0700

    Main program working.
    Still untested.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 import Citeproc
 import Citeproc.CslJson
-import Data.Text (Text)
+import Data.Text (Text, unpack)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Aeson as Aeson
 import System.IO
@@ -13,9 +14,19 @@ main = do
   bs <- BL.getContents
   case Aeson.eitherDecode bs of
     Left e -> err e
-    Right (inp :: Inputs (CslJson Text)) ->
-      print inp
-
+    Right (inp :: Inputs (CslJson Text)) -> do
+      parseResult <-
+        parseStyle (\_ -> return mempty) (inputsStylesheet inp)
+      case parseResult of
+        Left e -> err (unpack $ prettyCiteprocError e)
+        Right parsedStyle -> do
+           BL.putStr $ Aeson.encode $ citeproc
+                                        defaultCiteprocOptions
+                                        parsedStyle
+                                        (inputsLang inp)
+                                        (inputsReferences inp)
+                                        (inputsCitations inp)
+           BL.putStr "\n"
 
 err :: String -> IO ()
 err s = do
